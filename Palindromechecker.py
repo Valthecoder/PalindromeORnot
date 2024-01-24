deab0fbc732d07a73768abf440e7322e3e6d0bb0
ValtheCoder <keshia.kdf@gmail.com>
2024-01-24 21:37:17 +0800

    IF and Print Functions

--- Palindromechecker.py
+++ Palindromechecker.py
@@ -3,6 +3,15 @@ num_1 = 121
 num_2 = 125
 # Print "original number + number)
 print("original number", num_1)
+# If function for Palindrome number
+if str(num_1) == str(num_1)[::-1]:
+    print("Yes. given number is palindrome number")
+else:
+    print("No. given number is not palindrome number")
+
 print("")
 print("original number", num_2)
-# If function for Palindrome number
+if str(num_2) == str(num_2)[::-1]:
+    print("Yes. given number is palindrome number")
+else:
+    print("No. given number is not palindrome number")
