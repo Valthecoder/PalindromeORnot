cda680d5e108ebe0ec70620390d04266a4eb915f
ValtheCoder <keshia.kdf@gmail.com>
2024-01-24 21:24:06 +0800

    Set Variable and Print

--- Palindromechecker.py
+++ Palindromechecker.py
@@ -1,3 +1,8 @@
 # Set Variable for number
+num_1 = 121
+num_2 = 125
 # Print "original number + number)
+print("original number", num_1)
+print("")
+print("original number", num_2)
 # If function for Palindrome number
